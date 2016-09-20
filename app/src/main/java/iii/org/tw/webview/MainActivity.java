5abe1eb2c39a52b9a02b2836ccf4cd57f7acdbdb
cf904d31 <cf904d31@gmail.com>
2016-09-20 10:46:28 +0800

    WebView

--- app/src/main/java/iii/org/tw/webview/MainActivity.java
+++ app/src/main/java/iii/org/tw/webview/MainActivity.java
@@ -2,12 +2,24 @@ package iii.org.tw.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView mesg;
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mesg = (TextView) findViewById(R.id.mesg);
+        webView = (WebView) findViewById(R.id.webView);
+        initWebview();
+
+    }
+
+    private void initWebview() {
+
     }
 }
